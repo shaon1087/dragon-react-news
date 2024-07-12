b29629c8e8e2c67dc778f346ad0ea30542fb8f4e
shaon 1087 <shaonsaha1087@gmail.com>
2024-07-13 03:13:33 +0600

    marque part added

---
+++ src/Pages/Home/BreakingNews/BreakingNews.jsx
@@ -0,0 +1,19 @@
+
+import Marquee from "react-fast-marquee";
+import { Link } from "react-router-dom";
+const BreakingNews = () => {
+    return (
+        <div className="flex border">
+            <button className="btn btn-secondary px-8">Latest</button>
+            <Marquee className="px-4" pauseOnHover={true} speed={150}>
+            <Link className="mr-10" to='/'>Match Highlights: Germany vs Spain — as it happened..</Link>
+            <Link className="mr-10" to='/'>Match Highlights: Germany vs Spain — as it happened..</Link>
+            <Link className="mr-10" to='/'>Match Highlights: Germany vs Spain — as it happened..</Link>
+            
+            </Marquee>
+            
+        </div>
+    );
+};
+
+export default BreakingNews;
