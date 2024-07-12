9259c5cdfb47cf05d06fdd0c71417babd2663a7d
shaon 1087 <shaonsaha1087@gmail.com>
2024-07-13 01:47:59 +0600

    home added

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import Header from "../Shared/Header/Header";
+import Navbar from "../Shared/Navbar/Navbar";
+
+
+const Home = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Navbar></Navbar>
+            <h2 className="text-3xl font-Poppins font-bold">This is Home</h2>
+        </div>
+    );
+};
+
+export default Home;
